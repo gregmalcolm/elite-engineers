30fad83a368afde837b21cd6662b109c64237f82
gregmalcolm <greg@gregmalcolm.com>
2016-06-12 22:17:38 -0400

    Think the redux is stuff is more or less set up right initiating component stock

--- src/containers/Component/RowContainer.js
+++ src/containers/Component/RowContainer.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../../actions/componentStockActions';
 import ComponentRow from '../../components/Component/Row';
 
-export default class ComponentTableBodyContainer extends React.Component {
-  constructor() {
-    super(...arguments);
-    this.props = { component: {} };
+class ComponentRowContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = {
+      component: {},
+      actions: props.actions.save,
+      componentStock: props.componentStock
+    };
   }
   render() {
     return (
       <ComponentRow
         component={this.props.component}
+        save={this.props.actions.save}
+        componentStock={this.props.componentStock}
       />
     );
   }
 }
+
+ComponentRowContainer.propTypes = {
+  actions: PropTypes.object.isRequired,
+  componentStock: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    componentStock: state.componentStockReducer
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ComponentRowContainer);
