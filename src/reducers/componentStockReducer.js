417722bccb3c7fba8722ea2d6fb47a7218c72df3
gregmalcolm <greg@gregmalcolm.com>
2016-06-15 12:12:18 -0400

    Hmm. apparently you can only have one container per dom tree. Ready to rearrange the funiture again

--- src/reducers/componentStockReducer.js
+++ src/reducers/componentStockReducer.js
@@ -8,10 +8,11 @@ import initialState from './initialState';
 export default function componentStockReducer(state = initialState.componentStock, action) {
   switch (action.type) {
     case SAVE:
-      // Make a copy because you can't change state
-      //return objectAssign({}, state, {updatedAt: dateHelper.getFormatedDateTime(new Date())});
-      return objectAssign({}, state, {updatedAt: new Date()});
-    default:
-      return state;
+      if (action.name === state.componentName) {
+        // Make a copy because you can't change state
+        //return objectAssign({}, state, {updatedAt: dateHelper.getFormatedDateTime(new Date())});
+        return objectAssign({}, state, {personalStock: action.personalStock, updatedAt: new Date()});
+      }
   }
+  return state;
 }
