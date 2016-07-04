463fb618be44a6325d5284a8d5cc0cedd5e6c1ba
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-07-04 17:10:00 -0400

    Header links to home page

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Heading = () => (
   <div className="row">
     <div id="header">
-      <h1>ELITE ENGINEERS</h1>
-      <img className="image engineer-header"/>
+      <a href="/">
+        <h1>ELITE ENGINEERS</h1>
+        <img className="image engineer-header"/>
+      </a>
     </div>
   </div>
 );
